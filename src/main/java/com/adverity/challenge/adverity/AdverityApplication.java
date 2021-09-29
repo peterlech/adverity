5ed434111f141defae82822dbe79a9190efb28be
Piotr Lechnio <p.lechnio@webellian.com>
2021-09-29 11:51:25 +0200

    init

---
+++ src/main/java/com/adverity/challenge/adverity/AdverityApplication.java
@@ -0,0 +1,13 @@
+package com.adverity.challenge.adverity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdverityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdverityApplication.class, args);
+	}
+
+}
